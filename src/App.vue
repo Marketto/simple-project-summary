1df1564d60923195bc6cab8af38cea4b678ec1b0
Marco Ricupero <marco.ricupero@gmail.com>
2019-11-28 12:23:07 +0100

    v 0.1.1

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  align-items: stretch;
 }
 .table.b-table {
   width: 100%;
