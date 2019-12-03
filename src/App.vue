3f6622740bdbca2a4fa8111498f995e8bda0917e
Marco Ricupero <marco.ricupero@gmail.com>
2019-12-03 12:07:38 +0100

    Implemented axios to retrieve details

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
     <h1>Summary</h1>
-    <h2>Projects</h2>
-    <Projects :src="projects"/>
-    <hr/>
-    <h2>Websites</h2>
-    <Websites :src="websites"/>
+    <section v-if="projects.length">
+      <h2>Projects</h2>
+      <Projects :src="projects"/>
+    </section>
+    <section v-if="websites.length">
+      <h2>Websites</h2>
+      <Websites :src="websites"/>
+    </section>
   </div>
 </template>
 <script>
 import Projects from './components/Projects.vue';
 import Websites from './components/Websites.vue';
-
-import { projects, websites } from './assets/list.json';
+import axios from 'axios';
 
 export default {
   name: 'simple-project-summary',
@@ -21,9 +23,17 @@ export default {
     Websites
   },
   data: ()=> ({
-    projects,
-    websites
-  })
+    projects: [],
+    websites: []
+  }),
+  mounted() {
+    return axios
+      .get('assets/list.json')
+      .then(({ data: { projects, websites } }) => {
+        this.projects = projects;
+        this.websites = websites;
+      });
+  }
 }
 
 </script>
@@ -39,7 +49,22 @@ export default {
   flex-direction: column;
   justify-content: stretch;
   align-items: stretch;
+  padding: 1em 0;
 }
+h1 {
+  color: lightskyblue;
+}
+section {
+  margin: 1em 0;
+}
+section:not(:first-of-type) {
+  border-top: 2px solid lightskyblue;
+  padding-top: 1em;
+}
+section > h2 {
+  padding: 0.4em 0;
+}
+
 .table.b-table {
   width: 100%;
 }
